5f9ff1df9a778686a4ba216fc3891e692b036b04
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-06-10 23:09:24 +0900

    デザインの整形+バグの修正 #16

--- routes/api/imageCollector.js
+++ routes/api/imageCollector.js
@@ -1,23 +1,33 @@
 const twitter = require('twitter');
 const client = new twitter(require('../../token.json'));
+const Long = require('long');
 
 const tweetPush = (image, tweets) => {
+    let minId;
     for (let tweet of tweets) {
         if (tweet.extended_entities) {
             for (let media of tweet.extended_entities.media) {
                 image.push(media.media_url);
             }
         }
+        minId = tweet.id_str;
+
     }
+    if (typeof minId === 'undefined') {
+        return null;
+    }
+    return Long.fromString(minId).subtract(1).toString();
 };
 
 module.exports = (id) => {
     return new Promise((resolve, reject) => {
+        const searchId = id;
+        const loopNum = 16;
         let image = [];
         let count = 0;
         let maxId;
         let params = {
-            screen_name: id,
+            screen_name: searchId,
             count: 200,
             max_id: maxId,
             include_rts: false
@@ -26,17 +36,17 @@ module.exports = (id) => {
         let interval = setInterval(() => {
             client.get('statuses/user_timeline', params, (err, tweets, res) => {
                 if (!err) {
-                    tweetPush(image, tweets);
-                    params.max_id = tweets[tweets.length - 1].id - 1;
+                    params.max_id = tweetPush(image, tweets);
+                    count++;
+                    if (params.max_id === null) {
+                        count = loopNum;
+                    }
                 }
-                count++;
             });
-            setTimeout(() => {
-                if (count == 16) {
-                    resolve(image);
-                    clearInterval(interval);
-                }
-            }, 1000);
+            if (count === loopNum) {
+                resolve(image);
+                clearInterval(interval);
+            }
         }, 1000);
     });
 };
