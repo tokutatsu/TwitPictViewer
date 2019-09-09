1e520d5489f9745bc88c52c43d9d4d6449b68b07
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-09-09 22:29:22 +0900

    GETをPOSTへと変更 #44

--- routes/router.js
+++ routes/router.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const imageCollector = require('./api/imageCollector.js');
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
-router.get('/viewer', (req, res) => {
+router.post('/viewer', (req, res) => {
     res.render('viewer', {
-        id: req.query.id
+        id: req.body.id
     });
 });
 
